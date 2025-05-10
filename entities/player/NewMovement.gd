bc3ef34776bf50622de919e3a29f6f1ce8dd119c
RadicalGitter <oscar.bqt@gmail.com>
2025-05-10 19:07:41 +0200

    movement animation works!!

--- entities/player/NewMovement.gd
+++ entities/player/NewMovement.gd
@@ -4,16 +4,19 @@ var speed = 300  # move speed in pixels/sec
 var rotation_speed = 0.8  # turning speed in radians/sec
 var rotation_speed_multiplier = 1
 
-	
-
 
 func _physics_process(delta):
 	var move_input = Input.get_axis("move_down", "move_up")
 	var rotation_direction = Input.get_axis("move_left", "move_right")
 	velocity = transform.x * move_input * speed
 	rotation += rotation_direction * rotation_speed * delta * rotation_speed_multiplier
-	
 	move_and_slide()
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.play("walk-left")
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.play("walk-right")
+	if 	Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.play("walk-up")
 
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
