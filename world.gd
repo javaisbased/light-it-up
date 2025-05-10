235112efba8846f15f1afb6b39694095d5f53269
RadicalGitter <oscar.bqt@gmail.com>
2025-05-10 15:38:39 +0200

    Adding free music

--- world.gd
+++ world.gd
@@ -7,3 +7,7 @@ func _on_player_translate_world(direction, amount):
 	# The player intends to move by the vector (direction * amount).
 	# To achieve this with a fixed player, the world must move by the inverse vector.
 	self.position -= direction * amount
+
+
+func _on_audio_stream_player_2d_finished() -> void:
+	pass # Replace with function body.
